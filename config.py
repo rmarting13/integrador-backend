813fa493fc4b792a181f7671bb3ada59b044aa45
Martin Gonzalez <rmarting13@gmail.com>
2023-09-11 17:40:40 -0300

    012 - MySQL database connection successfully integrated

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
     SERVER_NAME = "127.0.0.1:5000"
     DEBUG = True
+    DATABASE_NAME = 'chatify'
     DATABASE_USERNAME = 'admin'
     DATABASE_PASSWORD = 'admin'
     DATABASE_HOST = 'localhost'
