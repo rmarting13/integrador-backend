d52ee47b5a4be3b544b1a0ec6c79fa4694f5c544
nahuelBarrera <nagusalta1@gmail.com>
2023-09-12 12:23:05 -0300

    tarea 011 y 013

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 def init_app():
     """Crea y configura la aplicación Flask"""
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, supports_credentials=True)
     # TODO: Register every controller's blueprint
 
     return app
