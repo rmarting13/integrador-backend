bed8552937243ad7e8bcb7edb624b2d90ffec827
Martin Gonzalez <rmarting13@gmail.com>
2023-09-13 02:23:09 -0300

    0620 - Exception Forbiden 403 created

--- app/models/exceptions.py
+++ app/models/exceptions.py
@@ -1,2 +1,19 @@
 from werkzeug.exceptions import HTTPException
+from flask import jsonify
+
+
+class Forbiden(HTTPException):
+    def __init__(self, description = "Not allowed for this user"):
+        super().__init__(description)
+        self.status_code = 403
+
+    def get_response(self):
+        response = jsonify({
+            'error': {
+            'code': self.code,
+            'description': self.description,
+            }
+        })
+        response.status_code = self.status_code
+        return response
 
